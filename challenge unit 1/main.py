51a953f86fc2600775cb92f2dde8d062caaa6724
surekha2005 <144364553+surekha2005@users.noreply.github.com>
2023-09-07 21:16:50 +0530

    Add files via upload

--- challenge unit 1/main.py
+++ challenge unit 1/main.py
@@ -1,21 +1,8 @@
-# Python program to check if year is a leap year or not
-
-year = 2000
-
-# To get year (integer input) from the user
-# year = int(input("Enter a year: "))
-
-# divided by 100 means century year (ending with 00)
-# century year divided by 400 is leap year
-if (year % 400 == 0) and (year % 100 == 0):
-    print("{0} is a leap year".format(year))
-
-# not divided by 100 means not a century year
-# year divided by 4 is a leap year
-elif (year % 4 ==0) and (year % 100 != 0):
-    print("{0} is a leap year".format(year))
-
-# if not divided by both 400 (century year) and 4 (not century year)
-#year is not a leap year
-else:
-  print("{0}is not a leap year".format(year))
+def fact_rec(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*fact_rec(n-1)
+number=2
+res = fact_rec(number)
+print("The factorial of {} is {}.".format(number,res))
